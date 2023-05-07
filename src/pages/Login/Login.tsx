d76e76d04cce25b3790023c27a9820bc017463a4
Hung Pham <hungco8923@gmail.com>
2023-05-07 18:11:13 +0700

    Login logic applied, optional notification placement

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,73 +1,135 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { Button, Checkbox, Form, Input, Divider, notification, Space } from 'antd'
-import React, { useMemo } from 'react'
-import type { NotificationPlacement } from 'antd/es/notification/interface'
+import { Button, Checkbox, Form, Input, Space } from 'antd'
+import { openNotification } from '../../util/notifications'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
-const Context = React.createContext({ name: 'Default' })
+const secret = 'your-secret-key'
 
 const Login = () => {
-  const [api, contextHolder] = notification.useNotification()
+  const navigate = useNavigate()
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  const openNotification = (placement: NotificationPlacement) => {
-    api.info({
-      message: `Notification ${placement}`,
-      description: <Context.Consumer>{({ name }) => `Hello, ${name}!`}</Context.Consumer>,
-      placement
-    })
+  const redirectHome = () => {
+    navigate('/')
   }
 
-  const contextValue = useMemo(() => ({ name: 'Ant Design' }), [])
+  const redirectRegistration = () => {
+    navigate('/register')
+  }
+
+  const redirectForgotPassword = () => {
+    navigate('/forgotPassword')
+  }
+
   const onFinish = (values: any) => {
-    console.log('Received values of form: ', values)
+    axios.get('https://644fb981ba9f39c6ab6aa25c.mockapi.io/mock/users', {}).then(res => {
+      res.data.forEach((user: any) => {
+        if (values.username === user.username && values.password === user.password) {
+          document.cookie = `username=${values.username}`
+        }
+      })
+    })
+    if (hasCookieKey('username')) {
+      setIsLoggedIn(true)
+    } else {
+      setIsLoggedIn(false)
+    }
+    if (isLoggedIn) {
+      openNotification(
+        {
+          message: 'Login Success',
+          description: `Hello ${values.username}`
+        },
+        'success',
+        'topLeft'
+      )
+      redirectHome()
+      console.log(isLoggedIn)
+    }
+    if (!isLoggedIn) {
+      openNotification(
+        {
+          message: 'Login Failed',
+          description: 'Wrong username or password. Please try again!'
+        },
+        'error',
+        'topLeft'
+      )
+    }
   }
+
+  function hasCookieKey(key: string): boolean {
+    const cookies = document.cookie.split(';')
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim()
+      if (cookie.startsWith(`${key}=`)) {
+        return true
+      }
+    }
+    return false
+  }
+
   return (
-    <div className="login">
-      <div className="login__breadcrumbs">
-        <p>Home / Account</p>
-      </div>
-      <div className="login__main">
-        <div className="login__main--form">
-          <h1>Login</h1>
-          <p>Please login using account detail below</p>
-          <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
-            <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
-            <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
-              <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-              <Form.Item name="remember" valuePropName="checked" noStyle>
-                <Checkbox>Remember me</Checkbox>
+    <>
+      <div className="login">
+        <div className="login__breadcrumbs">
+          <p>Home / Login</p>
+        </div>
+        <div className="login__main">
+          <div className="login__main--form">
+            <h1>Login</h1>
+            <p>Please login using account detail below</p>
+            <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
+              <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+              </Form.Item>
+              <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a className="login-form-forgot" onClick={redirectForgotPassword}>
+                  Forgot password
+                </a>
               </Form.Item>
 
-              <a className="login-form-forgot" href="">
-                Forgot password
-              </a>
-            </Form.Item>
-
-            <Form.Item>
-              <Context.Provider value={contextValue}>
-                {contextHolder}
+              <Form.Item>
                 <Space style={{ width: '100%' }} direction="vertical">
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    className="login-form-button"
-                    onClick={() => openNotification('topLeft')}
-                    block
-                  >
+                  <Button type="primary" htmlType="submit" className="login-form-button" block>
                     Log in
                   </Button>
                 </Space>
-              </Context.Provider>
-              Or <a href="">register now!</a>
-            </Form.Item>
-          </Form>
+                Or <a onClick={redirectRegistration}>register now!</a>
+              </Form.Item>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
+}
+
+const fetchData = async () => {
+  const token = localStorage.getItem('token')
+  try {
+    const response = await axios.get('/api/data', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export default Login
