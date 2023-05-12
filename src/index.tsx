dfc1e30147d1ea1127863210d91e7b2cf10280d0
Phan Hieu <phanbahieu460@gmail.com>
2023-05-12 08:38:41 +0700

    Setup redux redux-saga

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './assets/sass/style.scss'
 import { BrowserRouter } from 'react-router-dom'
+// import store from './redux/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
